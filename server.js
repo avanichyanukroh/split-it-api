21256155731abf0d09cef0983f6e8de29f66c42c
Anawin Vanichyanukroh <avanichyanukroh@gmail.com>
2019-03-31 04:57:55 -0700

    Modify configs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const router = express.Router();
 const bodyParser = require('body-parser');
 const app = express();
+const router = express.Router();
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
