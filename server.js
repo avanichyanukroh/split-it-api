ddeec578a6e82d97e2f829dc8faf559797ce78f7
Anawin Vanichyanukroh <avanichyanukroh@gmail.com>
2019-03-31 04:21:12 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+const app = express();
+const dotenv = require('dotenv');
+const googleApplicationCredentials = require('./split-it-f37c85ac3cc6.json');
+const port = process.env.PORT;
+const admin = require('firebase-admin');
+
+dotenv.config();
+app.use(bodyParser.json());
+
+admin.initializeApp({
+  credential: admin.credential.cert(googleApplicationCredentials),
+  databaseURL: 'https://split-it-da392.firebaseio.com/'
+});
+
+const db = admin.firestore();
+
+router.post('/api/mapJSON', (req, res) => {
+  const mappedData = mapReceipt(req.body);
+  console.log('mappedData: ', mappedData);
+  res.status(201).send(mappedData);
+});
+
+function mapReceipt(data) {
+
+  let textAnnotations = data.responses[0].textAnnotations;
+
+  let mapOutput ={
+      receipt: {}
+  };
+  let itemNameCount = 1;
+  let itemPriceCount = 1;
+  let rowPosition = 0;
+  let rowQueue = [];
+  let isCurrency = false;
+
+  for (let i = 1; i < textAnnotations.length; i++) {
+      if ((Math.abs(textAnnotations[i].boundingPoly.vertices[0].y - rowPosition) > 4 || i <= 1 ) && isCurrency === false) {
+          rowPosition = textAnnotations[i].boundingPoly.vertices[0].y;
+          rowQueue = [];
+          isCurrency = false;
+      }
+
+      if ((Math.abs(textAnnotations[i].boundingPoly.vertices[0].y - rowPosition) > 4 || i <= 1 ) && isCurrency === true) {
+          mapOutput.receipt[`item${itemPriceCount++}`].basePrice = rowQueue.join('');
+          
+          rowPosition = textAnnotations[i].boundingPoly.vertices[0].y;
+          rowQueue = [];
+          isCurrency = false;
+      }
+
+      if (textAnnotations[i].description === '$') {
+
+          if (rowQueue.join(' ') === '') {
+              break;
+          }
+          
+          mapOutput.receipt[`item${itemNameCount++}`] = {
+              name: rowQueue.join(' ')
+          }
+          rowQueue = [];
+          isCurrency = true;
+          continue;
+      }
+
+      rowQueue.push(textAnnotations[i].description);
+  }
+  
+  let grandTotal = 0;
+  let currentTotal;
+  for (let i = 1; i < itemNameCount; i++) {
+      mapOutput.receipt[`item${i}`]['fees'] = Math.floor(mapOutput.receipt[`item${i}`].basePrice * 0.098 * 100) / 100;
+      currentTotal = parseFloat(mapOutput.receipt[`item${i}`].basePrice) + ((Math.floor(mapOutput.receipt[`item${i}`].basePrice * 0.098 * 100) / 100));
+      grandTotal += currentTotal
+      mapOutput.receipt[`item${i}`]['total'] = currentTotal;
+  }
+
+  mapOutput.receipt['grandTotal'] = Math.ceil(grandTotal * 100) / 100;
+  
+  return mapOutput;
+}
+
+app.listen(port, () => {
+  console.log('Live and ready to hack! Listening on port: ' + port);
+});
