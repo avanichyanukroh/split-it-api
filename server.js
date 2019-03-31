f62e23e19eec31bdcc2ca2335806f420809f28c7
Anawin Vanichyanukroh <avanichyanukroh@gmail.com>
2019-03-31 05:37:18 -0700

    Update body parser config

--- server.js
+++ server.js
@@ -8,8 +8,19 @@ dotenv.config();
 const port = Number(process.env.PORT || 3000);
 
 app.use(express.json());
-app.use(express.bodyParser());
 app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
 app.get('/', (req, res) => {
   res.send({
